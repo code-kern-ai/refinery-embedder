09c60e4f5e708e2572e77f78b4f4979602e5dfb1
Felix Kirsch <40427105+FelixKirsch@users.noreply.github.com>
2022-09-05 17:38:16 +0200

    integrates model-provider service (#9)
    * integrates model-provider service
    
    * adds project_id to model-provider request
    
    * removes project_id and revision from model-provider api calls
    
    * try auto select model dir
    
    * adds error message if model is deleted during creation
    
    Co-authored-by: JWittmeyer <jens.wittmeyer@onetask.ai>

--- util/request_util.py
+++ util/request_util.py
@@ -2,6 +2,7 @@ import os
 import requests
 
 NEURAL_SEARCH_BASE_URI = os.getenv("NEURAL_SEARCH")
+MODEL_PROVIDER_BASE_URI = os.getenv("MODEL_PROVIDER")
 
 
 def post_embedding_to_neural_search(project_id: str, embedding_id: str) -> None:
@@ -17,3 +18,12 @@ def delete_embedding_from_neural_search(embedding_id: str) -> None:
     url = f"{NEURAL_SEARCH_BASE_URI}/delete_collection"
     params = {"embedding_id": embedding_id}
     requests.put(url, params=params)
+
+
+def get_model_path(model_name: str) -> str:
+    url = f"{MODEL_PROVIDER_BASE_URI}/model_path"
+    params = {
+        "model_name": model_name,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
