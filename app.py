ef3dfadfd50fbdd5431970466d589692ee5ae0e0
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-09 10:33:59 +0100

    Adds status code

--- app.py
+++ app.py
@@ -138,11 +138,12 @@ def config_changed() -> responses.PlainTextResponse:
 @app.get("/healthcheck")
 def healthcheck() -> responses.PlainTextResponse:
     text = ""
+    status_code = 200
     database_test = general.test_database_connection()
     if not database_test.get("success"):
         error_name = database_test.get("error")
         text += f"database_error:{error_name}:"
-
+        status_code = 500
     if not text:
         text = "OK"
-    return responses.PlainTextResponse(text)
+    return responses.PlainTextResponse(text, status_code=status_code)
