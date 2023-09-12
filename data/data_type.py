98ab81883a1ebcd1d61e097226dff8f626c64b29
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2023-09-12 11:38:31 +0200

    Option to edit records (#60)
    * Rembed endpoint
    
    * fixes token level embedding calculation
    
    ---------
    
    Co-authored-by: FelixKirschKern <felix.kirsch@kern.ai>

--- data/data_type.py
+++ data/data_type.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from typing import Optional
+from typing import Dict, List
 from pydantic import BaseModel
 
 
@@ -7,3 +6,11 @@ class EmbeddingRequest(BaseModel):
     project_id: str
     embedding_id: str
 
+
+class EmbeddingRebuildRequest(BaseModel):
+    # example request structure:
+    # {"<embedding_id>":[{"record_id":"<record_id>","attribute_name":"<attribute_name>","sub_key":<sub_key>}]}
+    # note that sub_key is optional and only for embedding lists relevant
+    # also sub_key is an int but converted to string in the request
+
+    changes: Dict[str, List[Dict[str, str]]]
