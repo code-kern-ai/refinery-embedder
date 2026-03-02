775ea3d38c6f13b5bdab2dd25455fe010824dedb
andhreljaKern <andrea.hrelja@kern.ai>
2026-03-02 19:21:35 +0100

    fix: apply trim length

--- src/embedders/classification/contextual.py
+++ src/embedders/classification/contextual.py
@@ -147,7 +147,7 @@ class OpenAISentenceEmbedder(SentenceEmbedder):
     ) -> Generator[List[List[float]], None, None]:
         for documents_batch in util.batch(documents, self.batch_size):
             documents_batch = [
-                doc.replace("\n", " ") for doc in filter(None, documents_batch)
+                self._trim_length(doc.replace("\n", " ")) for doc in documents_batch
             ]
             try:
                 if self.use_azure:
