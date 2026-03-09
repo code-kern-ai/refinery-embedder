d65c58a5e24398205be0da5dafe24dfdff3d2cd7
andhreljaKern <andrea.hrelja@kern.ai>
2026-03-09 10:49:25 +0100

    fix: filter empty embedding inputs

--- src/embedders/classification/contextual.py
+++ src/embedders/classification/contextual.py
@@ -147,7 +147,8 @@ class OpenAISentenceEmbedder(SentenceEmbedder):
     ) -> Generator[List[List[float]], None, None]:
         for documents_batch in util.batch(documents, self.batch_size):
             documents_batch = [
-                self._trim_length(doc.replace("\n", " ")) for doc in documents_batch
+                self._trim_length(doc.replace("\n", " "))
+                for doc in filter(None, documents_batch)
             ]
             try:
                 if self.use_azure:
